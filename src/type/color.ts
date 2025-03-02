821ed5664bad83b92790fd658a749377d08c1978
ewauq <emilien.wauquier@gmail.com>
2025-03-02 17:27:52 +0100

    Remove old files

--- src/type/color.ts
+++
@@ -1,1 +0,0 @@
-export type RGBColor = `rgb(${number}, ${number}, ${number})`
--- src/type/theme.ts
+++
@@ -1,3 +0,0 @@
-export type ThemeName = 'vibrant' | 'rainbow' | 'neon' | 'colorBlind'
-export type ThemeStyleName = 'blend' | 'gradual'
-export type ThemeColors = { [themeName: string]: `#${string}`[] }
