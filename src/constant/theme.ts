f5dbf6326d60b662b3058621e695b8bf250862fb
ewauq <emilien.wauquier@gmail.com>
2025-03-09 21:37:46 +0100

    Update the colors

--- src/constant/theme.ts
+++ src/constant/theme.ts
@@ -8,17 +8,17 @@ type Theme = { [key in ThemeName]: ThemeStyleValue }
 
 // To match with the rating step, the theme should have 5 colors or 10.
 export const themes: Theme = {
-  default5: ['#dc231c', '#fd7902', '#fdd514', '#62bd21', '#018ea6'],
+  default5: ['#ea4335', '#f27f1d', '#fbbc04', '#98b22b', '#34a853'],
   default10: [
-    '#dc231c',
-    '#f03c03',
-    '#fd7902',
-    '#fda506',
-    '#fdd514',
-    '#62bd21',
-    '#0b9e53',
-    '#018ea6',
-    '#015ca1',
-    '#163993',
+    '#ea4335',
+    '#ee6129',
+    '#f27f1d',
+    '#f69d11',
+    '#fbbc04',
+    '#cab717',
+    '#98b22b',
+    '#66ad3f',
+    '#34a853',
+    '#34a853',
   ],
 }
